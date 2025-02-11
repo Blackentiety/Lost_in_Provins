9f86e86b42bcd49684edb8ae950c4182fa6722a8
glassive <136472583+glassive@users.noreply.github.com>
2025-02-11 16:41:40 +0100

    progres

--- Assets/Script/Camera/cameraTransition.cs
+++ Assets/Script/Camera/cameraTransition.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
 using UnityEngine;
 
 public class doorTrigger : MonoBehaviour
@@ -10,10 +11,13 @@ public class doorTrigger : MonoBehaviour
     public float playerMoveY;
     void Start()
     {
+
     }
     void OnTriggerEnter2D(Collider2D col)
     {
-        Camera.main.transform.Translate(cameraMoveX, cameraMoveY, 0);
-        GameObject.FindWithTag("Player").transform.Translate(playerMoveX, playerMoveY, 0);
+            Time.timeScale = 0;
+            Camera.main.transform.position = new Vector3(cameraMoveX, cameraMoveY, -10);
+            GameObject.FindWithTag("Player").transform.Translate(playerMoveX, playerMoveY, 0);
+            Time.timeScale = 1;
     }
 }
